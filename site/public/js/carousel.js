7a1e4fac7516f1cb2f205b95cb395d211cb4d197
JuliaHown <juliahown87@gmail.com>
2023-06-04 11:13:53 -0300

    Arrumando funcionalidades do carousel

--- site/public/js/carousel.js
+++ site/public/js/carousel.js
@@ -3,35 +3,11 @@ firstImg = carousel.querySelectorAll("img")[0];
 arrowIcons = document.querySelectorAll(".wrapper i");
 
 let isDragStart = false, prevPageX, prevScrollLeft;
-let firstImgWidth = firstImg.clientWidth + 14; 
-// A primeira imagem tem largura e margem de valor 14
+let firstImgWidth = firstImg.clientWidth + 14; // Largura e margem fixa em 14 pixels
+
 
 arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
+    icon.addEventListener("click", () => { // Executou a função
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
     })
 });
-
-const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
-
-const dragging = (e) => {
-    // rolando as imagens para a esquerda de acordo com o cursor do mouse
-    if(!isDragStart) return;
-    e.preventDefault();
-    carousel.classList.add("dragging");
-    let positionDiff = e.pageX - prevPageX
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-}
-
-const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
-}
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("mouseup", dragStop);
