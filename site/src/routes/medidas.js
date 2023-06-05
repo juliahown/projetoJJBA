8d95d2188bafc708188b393c5c3ae6394f9ccce5
JuliaHown <juliahown87@gmail.com>
2023-06-04 22:13:00 -0300

    Atualizando medidas.js

--- site/src/routes/medidas.js
+++ site/src/routes/medidas.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 var medidaController = require("../controllers/medidaController");
 
-router.get("/ultimas/:idAquario", function (req, res) {
+router.get("/ultimas/:idUsuario", function (req, res) {
     medidaController.buscarUltimasMedidas(req, res);
 });
 
-router.get("/tempo-real/:idAquario", function (req, res) {
+router.get("/tempo-real/:idUsuario", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
+});
+
+router.get("/primeiras/:idUsuario", function (req, res) {
+    medidaController.buscarPrimeirasMedidas(req, res);
 })
 
 module.exports = router;
